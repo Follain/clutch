0370da92cfe12140730a0df7b096c545edfba19a
Samuel Martini <samueljmartini@gmail.com>
2017-02-02 17:58:19 +0100

    Fetch from .env test when rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "clutch"
 require "pp"
 
+Dotenv.overload(".env.test")
+
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
